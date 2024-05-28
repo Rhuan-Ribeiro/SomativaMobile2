0e1b6da6d2568c2524087566faec4a704e2283d9
Rhuan Ribeiro <rhuan.augusto.ribeiro@gmail.com>
2024-05-28 19:53:15 -0300

    initial commit

---
+++ mange_flix/android/app/src/main/kotlin/navegator/bar/example/mange_flix/MainActivity.kt
@@ -0,0 +1,5 @@
+package navegator.bar.example.mange_flix
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ mange_flix/lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:mange_flix/login.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: Login(),
+  ));
+}
